abce472e7950840c67b4a8f56124649878d297e0
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-29 19:31:19 +0530

    Valadation of Mobile Number

--- UserRegestration.java
+++ UserRegestration.java
@@ -10,6 +10,7 @@ public class UserRegestration {
 	public static void main(String[] args) {
 	
 		String nameRejex = "^[A-Z]{1}+[A-Za-z]{2,}";
+		String mobileNumberRejex = "[+]{0,1}+91+[ ]+[1-9]{1}+[0-9]{9}";
 		String emailRejex = "[a-zA-Z]{1,}?[A-Za-z0-9]{0,1}+([-._+]{0,1}+[a-zA-Z0-9]{1,}){0,1}"
 							+ "+@+[a-zA-Z0-9]{1,}?([-._]{0,1}+[a-zA-Z0-9]{2,}){0,1}?([-._]{0,1}"
 							+ "+[a-zA-Z]{2,}){0,1}";
@@ -25,6 +26,10 @@ public class UserRegestration {
 		System.out.print("Enter Email : ");
 		String email = scan.next();
 		validate(email, emailRejex);
+		
+		System.out.print("Enter Mobile Number : ");
+		String mobileNumber = scan.nextLine();
+		validate(mobileNumber, mobileNumberRejex);
 	}
 	
 	public static void validate(String validationString, String pattern) {
